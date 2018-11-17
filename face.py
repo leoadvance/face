2cb1a36131ff2de82c7ab550efed5552b27579d3
leo <leo_advance@163.com>
2018-11-17 16:06:53 +0800

    take picture

--- face.py
+++ face.py
@@ -6,11 +6,15 @@ import cv2
 print(cv2.__version__)
 
 camera = PiCamera()
-#camera.start_pr
 camera.resolution = (1920, 1080)
 camera.start_preview()
+print("start recording......")
 camera.start_recording('foo.h264')
 camera.wait_recording(6)
 camera.stop_recording()
+print("stop record!")
+sleep(1)
+print("Taking pictures")
+camera.capture('foo.jpg')
 camera.stop_preview()
 print("finish!")
